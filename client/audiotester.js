ab90d1477839349c29c194b3b995b2cf46ecab0e
PainedPsyche <pained.psyche@protonmail.com>
2024-05-14 20:18:24 +0200

    basic functionality

--- client/audiotester.js
+++ client/audiotester.js
@@ -1,6 +1,35 @@
 import alt from 'alt-client';
+import native from 'natives';
+
 import view from './view.js';
 
+const soundList = JSON.parse(alt.File.read('./data/soundNames.json'));
+
 alt.onServer('system:BeginGameplay', function() {
+    view.emit('debug:audio:LoadData', soundList);
+});
+
+alt.onServer('debug:AudioTester', function() {
+    alt.emit('debug:audio:Show');
+});
+
+alt.on('debug:audio:Show', function() {
     view.emit('debug:audio:Show');
+    view.focus();
+    alt.toggleGameControls(false);
+    alt.showCursor(true);
+});
+
+view.on('debug:audio:DataLoaded', function() {
+});
+
+view.on('debug:audio:Play', function(name, ref) {
+    native.playSoundFrontend(-1, name, ref, 1);
+});
+
+view.on('debug:audio:Close', function() {
+    view.emit('debug:audio:Hide');
+    view.unfocus();
+    alt.toggleGameControls(true);
+    alt.showCursor(false);
 });
--- ui/src/components/AudioTester.svelte
+++ ui/src/components/AudioTester.svelte
@@ -1,5 +1,33 @@
 <script>
     let visible = false;
+    let dataLoaded = false;
+    let soundList = [];
+    let selected;
+
+    async function loadData(data)
+    {
+        if(!dataLoaded)
+        {
+            soundList = data;
+            dataLoaded = true;
+
+            alt.emit("debug:audio:DataLoaded");
+        }
+    }
+
+    function play()
+    {
+        if(selected)
+        {
+            alt.emit("debug:audio:Play", selected.AudioName, selected.AudioRef);
+        }
+    }
+
+    function close()
+    {
+        hide();
+        alt.emit("debug:audio:Close");
+    }
 
     function show()
     {
@@ -11,12 +39,19 @@
         visible = false;
     }
 
+    alt.on("debug:audio:LoadData", loadData);
     alt.on("debug:audio:Show", show);
     alt.on("debug:audio::Hide", hide);
 </script>
 
 <div class="audio-tester-container" class:hidden={!visible}>
-    Hello, world!
+    <select bind:value={selected} name="audio-list" class="audio-tester-dropdown">
+        {#each soundList as sound}
+            <option value={sound}>Name: {sound.AudioName}, Ref: {sound.AudioRef}</option>
+        {/each}
+    </select>
+    <button on:click={play}>Play</button>
+    <button on:click={close}>Close</button>
 </div>
 
 <style>
@@ -26,5 +61,24 @@
         -webkit-font-smoothing: antialiased;
         -webkit-user-select: none;
         -webkit-user-drag: none;
+    }
+
+    .audio-tester-container.hidden {
+        display: none !important;
+    }
+
+    .audio-tester-container {
+        width: 50%;
+        height: 10%;
+        position: absolute;
+        left: 50%;
+        bottom: 50%;
+        transform: translate(-50%, -50%);
+        background-color: rgba(0, 0, 0, 0.5);
+        display: flex;
+        flex-direction: column;
+        gap: 10%;
+        justify-content: center;
+        align-items: center;
     }
 </style>
